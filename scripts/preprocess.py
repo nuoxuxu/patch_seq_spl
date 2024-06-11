6f5200fe278c6ba10df84b0b88f59de14989d986
Nuo Xu <xv.nuo@icloud.com>
2024-06-11 11:02:07 -0400

    modify preprocess.py to use class methods

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,14 +1,15 @@
 from patch_seq_spl.helper_functions import filter_adata, update_intron_group_size
 import scanpy as sc
 import pandas as pd
+import patch_seq_spl.helper_functions as src
 
-# filter
 
 adata = sc.read_h5ad(snakemake.input.adata_path)
-adata = filter_adata(
-    adata,
-    (snakemake.config["min_global_SJ_counts"], snakemake.config["min_cells_per_feature"], snakemake.config["min_cells_per_intron_group"]))
+adata = src.ExtendedAnnData(adata)
 
-adata = update_intron_group_size(adata)
+adata = adata.filter_adata((snakemake.config["min_global_SJ_counts"], snakemake.config["min_cells_per_feature"], snakemake.config["min_cells_per_intron_group"]))
+adata = adata.update_intron_group_size()
+
 adata.var.annotation = ((adata.var.gene_id_start != "") & (adata.var.gene_id_end != "")).astype(int)
+
 adata.write(snakemake.output[0])
