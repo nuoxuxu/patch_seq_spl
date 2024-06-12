be0c72f3b6afb7dfaa181019d882b764dc0d12e6
Nuo Xu <xv.nuo@icloud.com>
2024-06-12 01:24:05 -0400

    Remove function calling from transcript_viz.r

--- scripts/transcript_viz.r
+++ scripts/transcript_viz.r
@@ -1,17 +1,16 @@
 suppressMessages(
     {
-        library(ggtranscript)
-        library(magrittr)
         library(tidyverse)
         library(ggplot2)
         library(rtracklayer)
-        library(reticulate)
         library(stringr)
         library(glue)
     }
 )
 
-get_annotation_from_gtf <- function(gtf_path) {
+get_annotation_from_gtf <- function() {
+    GENOMIC_DATA_DIR <- Sys.getenv("GENOMIC_DATA_DIR")
+    gtf_path <- paste0(GENOMIC_DATA_DIR, "/Ensembl/Mouse/Release_110/Raw/Mus_musculus.GRCm39.110.gtf")
     annotation_from_gtf <- rtracklayer::import(gtf_path)
     annotation_from_gtf <- subset(annotation_from_gtf, mcols(annotation_from_gtf)$type %in% c("exon", "CDS"))
     annotation_from_gtf <- annotation_from_gtf[mcols(annotation_from_gtf)$transcript_name %>% complete.cases(), ]
@@ -48,6 +47,7 @@ findAdjacent <- function(query, subject) {
 }
 
 get_base_plot <- function(annodation_gene_name) {
+    library(ggtranscript)
     exons <- subset(annodation_gene_name, mcols(annodation_gene_name)$type == "exon") %>%
         as.data.frame()
     cds <- subset(annodation_gene_name, mcols(annodation_gene_name)$type == "CDS") %>%
@@ -159,15 +159,3 @@ plot_per_gene <- function(x, adjacent_only = TRUE, focus = TRUE) {
         ggsave(glue("proc/figures/{gene_name}/{intron}/transcript_viz.png"))
     }
 }
-
-# sig_VGIC_SJ <- read.csv("sig_VGIC_SJ.csv")
-# intron_group_list <- sig_VGIC_SJ$event_name
-# gene_name_list <- sapply(str_split(intron_group_list, "_"), `[`, 1)
-# intron_by_gene_name <- split(intron_group_list, gene_name_list)
-# lapply(intron_by_gene_name, plot_per_gene)
-
-GENOMIC_DATA_DIR <- "/Users/xunuo/Genomic_references"
-gtf_path <- paste0(GENOMIC_DATA_DIR, "/Ensembl/Mouse/Release_110/Raw/Mus_musculus.GRCm39.110.gtf")
-
-sig_intron_attr <- get_sig_intron_attr(sig_intron_attr)
-annotation_from_gtf <- get_annotation_from_gtf(gtf_path)
