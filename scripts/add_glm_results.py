d26c848c52adecdd8ffc7d64136c2ffeae673edd
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-11-09 14:11:49 -0500

    Refactor get_glm_results function to accept a list of CSV file paths and update main function to utilize snakemake input

--- scripts/add_glm_results.py
+++ scripts/add_glm_results.py
@@ -2,14 +2,14 @@ import anndata
 import pandas as pd
 from typing import Literal
 
-def get_glm_results(path: str, key: Literal["p_value", "statistic"] = "p_value"):
+def get_glm_results(glm_results_list: list, key: Literal["p_value", "statistic"] = "p_value"):
     """
     Get p-values or effect sizes from likelihood ratio test
     The csv file has to contain the columns "event_name" and "p_value"
 
     Args:
-        path: str
-            path to directory containing csv files
+        glm_results_list: list
+            list of paths to csv files
         key: str
             "p_value" or "statistic"
 
@@ -22,8 +22,8 @@ def get_glm_results(path: str, key: Literal["p_value", "statistic"] = "p_value")
     from pathlib import Path
     from statsmodels.stats.multitest import fdrcorrection
 
-    glm_results = dd.read_csv([path for path in Path(path).iterdir()], include_path_column = True)\
-        .pivot_table(index = "event_name", columns = "path", values = key).compute()
+    glm_results = dd.read_csv(glm_results_list, include_path_column = True)\
+        .pivot_table(index = "intron_group", columns = "path", values = key).compute()
     glm_results.rename(columns = {path: Path(path).stem for path in glm_results.columns}, inplace = True)
     glm_results = glm_results.dropna()
     #TODO Why does fdrcorrection turn quantas p values all into 1?
@@ -31,20 +31,13 @@ def get_glm_results(path: str, key: Literal["p_value", "statistic"] = "p_value")
         fdrcorrection(glm_results.values.flatten())[1].reshape(glm_results.shape), 
         index = glm_results.index, 
         columns = glm_results.columns)
-    
-    if "quantas" in path:
-        event_name_gene_name = pd.read_csv("data/quantas/Mm.seq.all.AS.chrom.can.id2gene2symbol", sep = "\t", header = None)\
-            .set_index(0)\
-            .loc[:, 2]\
-            .to_dict()        
-        glm_results["gene_name"] = glm_results.index.map(event_name_gene_name)
-        glm_results["gene_name"] = glm_results.groupby("gene_name").cumcount().add(1).astype(str).radd(glm_results["gene_name"] + '_')
-        glm_results = glm_results.set_index("gene_name")
-
     return glm_results
 
-# for group_by in ["three", "five"]:
-#     adata = anndata.read_h5ad(f"proc/scquint/preprocessed_adata_{group_by}.h5ad")
-#     adata.uns["simple"] = get_glm_results(f"proc/scquint/{group_by}/simple")
-#     adata.uns["multiple"] = get_glm_results(f"proc/scquint/{group_by}/multiple")
-#     adata.write_h5ad(f"proc/scquint/preprocessed_adata_{group_by}.h5ad")
+def main():
+    adata = anndata.read_h5ad(snakemake.input.adata_path)
+    adata.uns["simple"] = get_glm_results(snakemake.input.simple_result_list, "p_value")
+    adata.uns["multiple"] = get_glm_results(snakemake.input.multiple_result_list, "p_value")
+    adata.write_h5ad(snakemake.output[0])
+
+if __name__ == "__main__":
+    main()
